39a552d68cd6f9f6c8f0621a62e6ee1e65aa12d2
Nicolas Grimault <nicolas.g21@live.fr>
2018-02-10 18:12:53 +0100

    init project

---
+++ GameReplayViewer/Converter/BooleanToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace GameReplayViewer.Converter
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public bool IsInverse { get; set; }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool visibility = (bool)value;
+            if (IsInverse)
+            {
+                visibility = !visibility;
+            }
+
+            return visibility ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- GameReplayViewer/ViewModel/MainViewModel.cs
+++ GameReplayViewer/ViewModel/MainViewModel.cs
@@ -1,24 +1,57 @@
 ﻿using GameReplayViewer.Model;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
+using Prism.Commands;
 
 namespace GameReplayViewer.ViewModel
 {
     public class MainViewModel
     {
+        public MainViewModel()
+        {
+            Game Overwatch = new Game() { Name = "Overwatch", Path = "D:/Nicolas/Documents/Overwatch/videos/overwatch/" };
+            gameReplayItems = new List<GameReplay>() { new GameReplay() { Path = "soldier rocket 3kills_17-08-24_20-41-07.mp4", Name = "soldier rocket 3kills", Game = Overwatch } };
+            selectedGameReplay = gameReplayItems.First();
+            this.PlayCommand = new DelegateCommand(this.Play, this.CanBeResumed);
+            this.PauseCommand = new DelegateCommand(this.Pause, this.CanBePaused);
+
+        }
+
         private GameReplay selectedGameReplay;
         private List<GameReplay> gameReplayItems;
+        private bool isVideoPlaying = true;
 
+
+
+        private void Play() {
+
+        }
+        private void Pause()
+        {
+
+        }
+
+        public ICommand PlayCommand { get; private set; }
+        public ICommand PauseCommand { get; private set; }
+
+
+        public bool IsVideoPlaying
+        {
+            get { return isVideoPlaying; }
+        }
+        public bool IsVideoPaused
+        {
+            get { return !isVideoPlaying; }
+        }
+        private bool CanBePaused() => IsVideoPaused;
+        private bool CanBeResumed() => IsVideoPlaying;
 
         public GameReplay SelectedGameReplay
         {
             get { return selectedGameReplay; }
             set { selectedGameReplay = value; }
         }
-
 
         public List<GameReplay> GameReplayItems
         {
